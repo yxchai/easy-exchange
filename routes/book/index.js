0526db07efaa508638e01b56268dfe0c01fa9679
Woody Chai <cyxjimmy@gmail.com>
2012-05-02 22:24:57 +0800

    add more router export

--- routes/book/index.js
+++ routes/book/index.js
@@ -1,6 +1,8 @@
 module.exports = {
     add: require('./add'),
     insert: require('./insert'),
+    info: require('./info'),
+    edit: require('./edit'),
     update: require('./update'),
     del: require('./del'),
     getall: require('./getall')
